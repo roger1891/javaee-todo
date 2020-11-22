efb42572245191938c8ed4dd62811d010c45d200
Roger Braun <rogerrainersbraun@gmail.com>
2020-11-22 17:21:36 +0100

    initial commit

---
+++ src/main/java/academy/learnprogramming/entity/Todo.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package academy.learnprogramming.entity;
+
+import java.time.LocalDate;
+import javax.json.bind.annotation.JsonbDateFormat;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Roger
+ */
+
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty(message = "Task must be set")
+    @Size(min = 10, message = "Task should not be less than 10 characters")
+    private String task;
+
+
+    @NotNull(message = "Due must be set")
+    @FutureOrPresent(message = "Dude must be in the present or future")
+    @JsonbDateFormat(value = "yyyy-MM-dd")
+    private LocalDate dueDate;
+
+
+    private boolean isCompleted;
+    private LocalDate dateCompleted;
+    private LocalDate dateCreated;
+
+
+    @PrePersist
+    private void init() {
+        setDateCreated(LocalDate.now());
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isIsCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    public LocalDate getDateCompleted() {
+        return dateCompleted;
+    }
+
+    public void setDateCompleted(LocalDate dateCompleted) {
+        this.dateCompleted = dateCompleted;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+
+}
